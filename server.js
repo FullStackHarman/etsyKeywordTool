f1ca64a05bc93a574c9e1490f6594ac2f91378cf
Harman Preet Singh <harmanprt5142@gmail.com>
2024-08-01 08:01:56 +0530

    initiated bare minimum

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-let a = XMLDocument
+// Description: This is the server file for Etsy Project.
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+})
+
